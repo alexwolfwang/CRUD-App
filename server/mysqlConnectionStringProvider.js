a92169c4970682b0c9665361f7c15baceb733f06
alexwolfwang <alexwolfwang@gmail.com>
2016-12-04 23:12:20 -0500

    mysqlConnectionStringProvider upload

---
+++ server/mysqlConnectionStringProvider.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Alex.W on 2016/12/4.
+ */
+var mysql = require('mysql');
+
+var mysqlConnectionString = require('./mysqlConnectionString.js');
+
+var mysqlConnectionStringProvider = {
+
+    getMySqlConnection : function () {
+
+        var connection = mysql.createConnection(mysqlConnectionString.mysqlConnectionString.connection.dev);
+
+        connection.connect(function (err) {
+
+            if (err) { throw err; }
+
+            console.log('Connected Successfully');
+        });
+        return connection;
+    },
+
+    closeMySqlConnection : function (currentConnection) {
+
+        if (currentConnection) {
+
+            currentConnection.end(function (err) {
+
+                if (err) { throw err; }
+
+                console.log('connection closed successfully.')
+            })
+        }
+
+    }
+
+
+};
+
+module.exports.mysqlConnectionStringProvider = mysqlConnectionStringProvider;
